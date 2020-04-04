43231fd70f4c8d6ae4c1268147a8fc77c3deb88e
Manuel Juan Fosela Águila <manufosela@gmail.com>
2020-04-04 13:03:33 +0200

    [improvments] choose LICENSE. [Fixed] user and stop

--- generators/app/index.js
+++ generators/app/index.js
@@ -22,7 +22,7 @@ module.exports = class extends Generator {
       yosay(
         `Welcome to the ${chalk.red("lit-element-base")} generator (v1.3.8)!`
       )
-    );
+    )
 
     const prompts = [
       {
@@ -41,6 +41,12 @@ module.exports = class extends Generator {
         default: whoami
       },
       {
+        type: "input",
+        name: "license",
+        message: "LICENSE",
+        default: "Apache-2.0"
+      },
+      {
         type: "confirm",
         name: "start",
         message: "Would you like to create a lit-element webcomponent?",
@@ -51,109 +57,120 @@ module.exports = class extends Generator {
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
       this.props = props;
-      this.props.wcname = this.props.wcname.toLowerCase();
-      const { readFileSync, writeFileSync, mkdirSync } = require("fs");
-      const { join } = require("path");
-      try {
-        const { execSync } = require("child_process");
-        execSync("rm -rf " + path.join(__dirname, "output"));
-      } catch (e) {}
-      mkdirSync(join(__dirname, "output"));
-      mkdirSync(join(__dirname, "output", "demo"));
+      if (this.props.start) {
+        this.props.wcname = this.props.wcname.toLowerCase();
+        const { readFileSync, writeFileSync, mkdirSync } = require("fs");
+        const { join } = require("path");
+        try {
+          const { execSync } = require("child_process");
+          execSync("rm -rf " + path.join(__dirname, "output"));
+        } catch (e) {}
+        mkdirSync(join(__dirname, "output"));
+        mkdirSync(join(__dirname, "output", "demo"));
 
-      const filenames = [
-        "wc-name.js",
-        "index.html",
-        "demo/index.html",
-        "package.json",
-        "rollup.config.js",
-        "LICENSE",
-        "README.md"
-      ];
-      const propCamelCase = this.props.wcname
-        .split("-")
-        .map(part => {
-          return part.charAt(0).toUpperCase() + part.slice(1);
-        })
-        .join("");
-      filenames.forEach(filename => {
-        let content = readFileSync(
-          join(__dirname, "templates", filename),
-          "utf8"
-        );
-        let goodContent = content.replace(/wc-name/gm, this.props.wcname);
-        goodContent = goodContent.replace(/WcName/gm, propCamelCase);
-        if (filename === "wc-name.js") {
-          filename = this.props.wcname + ".js";
-        }
-        goodContent = goodContent.replace(/WcName/gm, propCamelCase);
-        goodContent = goodContent.replace(/"user"/gm, whoami);
-        writeFileSync(join(__dirname, "output", filename), goodContent);
+        const filenames = [
+          "wc-name.js",
+          "index.html",
+          "demo/index.html",
+          "package.json",
+          "rollup.config.js",
+          "LICENSE",
+          "README.md"
+        ];
+        const propCamelCase = this.props.wcname
+          .split("-")
+          .map(part => {
+            return part.charAt(0).toUpperCase() + part.slice(1);
+          })
+          .join("");
+        filenames.forEach(filename => {
+          let content = readFileSync(
+            join(__dirname, "templates", filename),
+            "utf8"
+          );
+          let goodContent = content.replace(/wc-name/gm, this.props.wcname);
+          goodContent = goodContent.replace(/WcName/gm, propCamelCase);
+          if (filename === "wc-name.js") {
+            filename = this.props.wcname + ".js";
+          }
+          goodContent = goodContent.replace(/WcName/gm, propCamelCase);
+          goodContent = goodContent.replace(/user/gm, this.props.author);
+          writeFileSync(join(__dirname, "output", filename), goodContent);
 
-        /** FILE package.json */
-        let packageContent = readFileSync(
-          join(__dirname, "templates", "package.json"),
-          "utf8"
-        );
-        let goodContent2 = packageContent.replace(/user/gm, whoami);
-        goodContent2 = goodContent2.replace(/wc-name/gm, this.props.wcname);
-        writeFileSync(join(__dirname, "output", "package.json"), goodContent2);
+          /** FILE package.json */
+          let packageContent = readFileSync(
+            join(__dirname, "templates", "package.json"),
+            "utf8"
+          );
+          let goodContent2 = packageContent.replace(/user/gm, this.props.author);
+          goodContent2 = goodContent2.replace(/wc-name/gm, this.props.wcname);
+          goodContent2 = goodContent2.replace(/LICENSE/gm, this.props.license);
+          writeFileSync(join(__dirname, "output", "package.json"), goodContent2);
 
-        /** FILE README.md */
-        let readmeContent = readFileSync(
-          join(__dirname, "templates", "README.md"),
-          "utf8"
-        );
-        let goodContent3 = readmeContent.replace(
-          /wc-name/gm,
-          this.props.wcname
-        );
-        writeFileSync(join(__dirname, "output", "README.md"), goodContent3);
-      });
+          /** FILE README.md */
+          let readmeContent = readFileSync(
+            join(__dirname, "templates", "README.md"),
+            "utf8"
+          );
+          let goodContent3 = readmeContent.replace(
+            /wc-name/gm,
+            this.props.wcname
+          );
+          writeFileSync(join(__dirname, "output", "README.md"), goodContent3);
+        });
+      }
     });
   }
 
   writing() {
-    const { join } = require("path");
-    this.fs.copy(
-      this.templatePath(join(__dirname, "output", this.props.wcname + ".js")),
-      this.destinationPath(this.props.wcname + ".js")
-    );
-    this.fs.copy(
-      this.templatePath(join(__dirname, "output", "package.json")),
-      this.destinationPath("package.json")
-    );
-    this.fs.copy(
-      this.templatePath(join(__dirname, "output", "index.html")),
-      this.destinationPath("index.html")
-    );
-    this.fs.copy(
-      this.templatePath(join(__dirname, "output", "demo/index.html")),
-      this.destinationPath("demo/index.html")
-    );
-    this.fs.copy(
-      this.templatePath(join(__dirname, "output", "rollup.config.js")),
-      this.destinationPath("rollup.config.js")
-    );
-    this.fs.copy(
-      this.templatePath(join(__dirname, "output", "LICENSE")),
-      this.destinationPath("LICENSE")
-    );
-    this.fs.copy(
-      this.templatePath(join(__dirname, "output", "README.md")),
-      this.destinationPath("README.md")
-    );
-    this.fs.copy(
-      this.templatePath(join(__dirname, "templates", "babel.config.js")),
-      this.destinationPath("babel.config.js")
-    );
-    this.fs.copy(
-      this.templatePath(join(__dirname, "templates", "gitignorefile")),
-      this.destinationPath(".gitignore")
-    );
+    if (this.props.start) {
+      const { join } = require("path");
+      this.fs.copy(
+        this.templatePath(join(__dirname, "output", this.props.wcname + ".js")),
+        this.destinationPath(this.props.wcname + ".js")
+      );
+      this.fs.copy(
+        this.templatePath(join(__dirname, "output", "package.json")),
+        this.destinationPath("package.json")
+      );
+      this.fs.copy(
+        this.templatePath(join(__dirname, "output", "index.html")),
+        this.destinationPath("index.html")
+      );
+      this.fs.copy(
+        this.templatePath(join(__dirname, "output", "demo/index.html")),
+        this.destinationPath("demo/index.html")
+      );
+      this.fs.copy(
+        this.templatePath(join(__dirname, "output", "rollup.config.js")),
+        this.destinationPath("rollup.config.js")
+      );
+      this.fs.copy(
+        this.templatePath(join(__dirname, "output", "LICENSE")),
+        this.destinationPath("LICENSE")
+      );
+      this.fs.copy(
+        this.templatePath(join(__dirname, "output", "README.md")),
+        this.destinationPath("README.md")
+      );
+      this.fs.copy(
+        this.templatePath(join(__dirname, "templates", "babel.config.js")),
+        this.destinationPath("babel.config.js")
+      );
+      this.fs.copy(
+        this.templatePath(join(__dirname, "templates", "gitignorefile")),
+        this.destinationPath(".gitignore")
+      );
+    } else {
+      this.log(
+        "\n\n* * *   STOPED Generator Lit Element Base by User   * * *\n"
+      );
+    }
   }
 
   install() {
-    this.npmInstall();
+    if (this.props.start) {
+      this.npmInstall();
+    }
   }
 };
--- generators/app/templates/rollup.config.js
+++ generators/app/templates/rollup.config.js
@@ -1,18 +1,18 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import terser from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
-	preserveSymlinks: true,
-	input: [ 'wc-name.js' ],
-	output: {
-		file: 'build/wc-name.js',
-		format: 'es',
-		sourcemap: true
-	},
-	plugins: [
-		resolve(),
-		babel(),
-		terser()
-	]
+  preserveSymlinks: true,
+  input: [ 'wc-name.js' ],
+  output: {
+    file: 'build/wc-name.min.js',
+    format: 'es',
+    sourcemap: true
+  },
+  plugins: [
+    resolve(),
+    babel(),
+    terser()
+  ]
 };
