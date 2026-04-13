manufosela/generator-litelement-webcomponent

commit 897b5096a977d83e15468410112492b06c4e7df4

fix: stop shadowing global process and persist dependency updates (#35)
- Import execSync from node:child_process directly instead of aliasing
  the whole child_process module as `process`, which hid the global
- Use node: prefix for path, url and fs for clarity
- Drop the nodeprocess workaround now that the global process is usable
- Persist replacePackageVersion output into this.packageJsonContent so
  devDependencies updates are no longer lost

generators/app/index.js
@@ -4,13 +4,12 @@ import chalk from "chalk";
 
 import Generator from "yeoman-generator";
 import yosay from "yosay";
-import path from "path";
-import process from "child_process";
-import nodeprocess from "node:process";
-import { fileURLToPath } from "url";
-import { readFileSync, writeFileSync } from "fs";
+import path from "node:path";
+import { execSync } from "node:child_process";
+import { fileURLToPath } from "node:url";
+import { readFileSync, writeFileSync } from "node:fs";
 
-const whoami = process.execSync("whoami").toString().replace(/\n/, "");
+const whoami = execSync("whoami").toString().replace(/\n/, "");
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -18,7 +17,7 @@ export default class extends Generator {
   /** * AUXILIARY METHODS */
   installWC() {
     try {
-      process.execSync("npm init @open-wc", { stdio: "inherit" });
+      execSync("npm init @open-wc", { stdio: "inherit" });
       console.log("npm init @open-wc ejecutado correctamente.");
     } catch (error) {
       console.error("Error al ejecutar npm init @open-wc:", error.message);
@@ -26,12 +25,9 @@ export default class extends Generator {
   }
 
   putIntoWCDirectory() {
-    const dir = process
-      .execSync("ls -t | head -n1")
-      .toString()
-      .replace(/\n/, "");
+    const dir = execSync("ls -t | head -n1").toString().replace(/\n/, "");
     console.log("\nDirectorio creado: " + dir);
-    nodeprocess.chdir(dir);
+    process.chdir(dir);
   }
 
   getWCName() {
@@ -79,7 +75,7 @@ export default class extends Generator {
 
   installNewPackageJson() {
     try {
-      process.execSync("npm install", { stdio: "inherit" });
+      execSync("npm install", { stdio: "inherit" });
       console.log("Dependencias actualizadas correctamente.");
     } catch (error) {
       console.error("Error al actualizar las dependencias:", error.message);
@@ -88,7 +84,7 @@ export default class extends Generator {
 
   fixPackageJsonVulnerabilities() {
     try {
-      process.execSync("npm audit fix", { stdio: "inherit" });
+      execSync("npm audit fix", { stdio: "inherit" });
       console.log("Vulnerabilidades arregladas correctamente.");
     } catch (error) {
       console.error("Error al arreglar las vulnerabilidades:", error.message);
@@ -178,18 +174,16 @@ export default class extends Generator {
 
   updateDependenciesVersions() {
     const jsonPackageParsed = JSON.parse(this.packageJsonContent);
-    let jsonStringNew = this.replacePackageVersion(
+    this.packageJsonContent = this.replacePackageVersion(
       this.packageJsonContent,
       jsonPackageParsed,
       "dependencies"
     );
-    jsonStringNew = this.replacePackageVersion(
+    this.packageJsonContent = this.replacePackageVersion(
       this.packageJsonContent,
       jsonPackageParsed,
       "devDependencies"
     );
-
-    return jsonStringNew;
   }
 
   /** * MAIN METHODS */
@@ -270,7 +264,7 @@ export default class extends Generator {
 
         /* Obtener directorio de trabajo del script */
         console.log("12.- Get working directory");
-        const directorioWC = nodeprocess.cwd();
+        const directorioWC = process.cwd();
 
         /* Generar fichero src/wc-name-style.js */
         console.log("13.- Generate WCStyleFile from template");
@@ -287,7 +281,7 @@ export default class extends Generator {
         this.log(
           "\n\n* * *   STOPED Generator Lit Element Base by User   * * *\n"
         );
-        nodeprocess.exit(0);
+        process.exit(0);
       }
     });
   }

commit f1de4dd66a8e6a3a808c1c6334a90fb5530a88bc

refactor(GLB-TSK-0002): mutate package.json via JSON parse/stringify
Replace regex-based string mutations of package.json with direct object
mutation on a parsed JSON representation, so the generator no longer
depends on the exact formatting produced by @open-wc.

- Read package.json into this.packageJson and serialize once on write
  (JSON.stringify(..., null, 2) + "\n").
- updatePackageJsonInfo / updateMoreInfoPackageJson / replacePackageVersion
  now mutate the object directly.
- Fix leftover process.execSync(npmCommand) call inside replacePackageVersion
  to use the imported execSync (was broken after PR #35).

generators/app/index.js
@@ -31,46 +31,22 @@ export default class extends Generator {
   }
 
   getWCName() {
-    const jsonPackage = JSON.parse(this.packageJsonContent);
-    console.log("Nombre del webcomponent: " + jsonPackage.name);
-    return jsonPackage.name;
+    console.log("Nombre del webcomponent: " + this.packageJson.name);
+    return this.packageJson.name;
   }
 
   updatePackageJsonInfo() {
-    const regExp = new RegExp('"author": "' + this.props.wcname + '"', "gm");
-    this.packageJsonContent = this.packageJsonContent.replace(
-      regExp,
-      '"author": "' + this.props.author + '"'
-    );
-    this.packageJsonContent = this.packageJsonContent.replace(
-      /"license": "MIT"/gm,
-      '"license": "' + this.props.license + '"'
-    );
-    this.packageJsonContent = this.packageJsonContent.replace(
-      /"version": "0.0.0"/gm,
-      '"version": "1.0.0"'
-    );
-    this.packageJsonContent = this.packageJsonContent.replace(
-      /"main": "index.js"/gm,
-      '"main": "' + this.props.wcname + '.js"'
-    );
+    this.packageJson.author = this.props.author;
+    this.packageJson.license = this.props.license;
+    this.packageJson.version = "1.0.0";
+    this.packageJson.main = `${this.props.wcname}.js`;
   }
 
   updateMoreInfoPackageJson() {
-    const home = `  "home": "https://github.com/${this.props.author}/${this.props.wcname}"`;
-    const repository = `  "repository": "git+https://github.com/${this.props.author}/${this.props.wcname}.git"`;
-    const bugs = `  "bugs": "https://github.com/${this.props.author}/${this.props.wcname}/issues"`;
-
-    this.packageJsonContent = this.packageJsonContent.replace(
-      '"customElements": "custom-elements.json",',
-      '"customElements": "custom-elements.json",\n' +
-        home +
-        ",\n" +
-        repository +
-        ",\n" +
-        bugs +
-        ","
-    );
+    const repoBase = `https://github.com/${this.props.author}/${this.props.wcname}`;
+    this.packageJson.home = repoBase;
+    this.packageJson.repository = `git+${repoBase}.git`;
+    this.packageJson.bugs = `${repoBase}/issues`;
   }
 
   installNewPackageJson() {
@@ -151,39 +127,25 @@ export default class extends Generator {
     writeFileSync(path.join(directorioWC, "LICENSE"), licenseContent);
   }
 
-  replacePackageVersion(jsonString, jsonPackage, deps) {
-    let jsonStringNew = jsonString;
-    const depsName = Object.keys(jsonPackage[deps]);
-    console.log(jsonPackage[deps]);
-    depsName.forEach((depName) => {
+  replacePackageVersion(deps) {
+    const depsMap = this.packageJson[deps];
+    if (!depsMap) {
+      return;
+    }
+    console.log(depsMap);
+    Object.keys(depsMap).forEach((depName) => {
       console.log("check version for " + depName);
       const npmCommand = `npm view ${depName} version`;
-      const packageVersion = process
-        .execSync(npmCommand)
-        .toString()
-        .replace(/\n/, "");
-      const searched =
-        '"' + depName + '": "' + jsonPackage[deps][depName] + '"';
-      const replacedby = '"' + depName + '": "^' + packageVersion + '"';
-      console.log("searched: " + searched);
-      console.log("replacedby: " + replacedby);
-      jsonStringNew = jsonStringNew.replace(searched, replacedby);
+      const packageVersion = execSync(npmCommand).toString().replace(/\n/, "");
+      const newValue = "^" + packageVersion;
+      console.log(`${depName}: ${depsMap[depName]} -> ${newValue}`);
+      depsMap[depName] = newValue;
     });
-    return jsonStringNew;
   }
 
   updateDependenciesVersions() {
-    const jsonPackageParsed = JSON.parse(this.packageJsonContent);
-    this.packageJsonContent = this.replacePackageVersion(
-      this.packageJsonContent,
-      jsonPackageParsed,
-      "dependencies"
-    );
-    this.packageJsonContent = this.replacePackageVersion(
-      this.packageJsonContent,
-      jsonPackageParsed,
-      "devDependencies"
-    );
+    this.replacePackageVersion("dependencies");
+    this.replacePackageVersion("devDependencies");
   }
 
   /** * MAIN METHODS */
@@ -228,7 +190,9 @@ export default class extends Generator {
 
         /* Leer el fichero package.json */
         console.log("3.- Read package.json");
-        this.packageJsonContent = readFileSync("package.json", "utf8");
+        this.packageJson = JSON.parse(
+          readFileSync("package.json", "utf8")
+        );
 
         /* Leer el fichero package.json y extraer el nombre del webcomponent de la propiedad name */
         console.log("4.- Get WC name from package.json");
@@ -248,7 +212,11 @@ export default class extends Generator {
 
         /* Escribir el neuvo fichero package.json */
         console.log("8.- Write new package.json");
-        writeFileSync("package.json", this.packageJsonContent, "utf8");
+        writeFileSync(
+          "package.json",
+          JSON.stringify(this.packageJson, null, 2) + "\n",
+          "utf8"
+        );
 
         /* Arreglar posibles vulnerabilidades del package.json */
         console.log("9.- Fix package.json vulnerabilities");